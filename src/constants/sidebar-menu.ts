4b6f6a12db581b483272c8e9dca8401b7f566778
Alish Baig <alishbaig2004@gmail.com>
2025-02-10 12:32:49 +0530

    feat: add constants for sidebar routes

---
+++ src/constants/sidebar-menu.ts
@@ -0,0 +1,44 @@
+import {
+  BiRuler,
+  BiSolidUserBadge,
+  BiSolidUserRectangle,
+  BiTask,
+} from "react-icons/bi";
+
+const STAFF_ROUTES = [
+  {
+    name: "All Staff",
+    path: "/staff",
+    icon: BiSolidUserRectangle,
+  },
+  {
+    name: "Roles",
+    path: "/staff/roles",
+    icon: BiSolidUserBadge,
+  },
+  {
+    name: "Tasks",
+    path: "/staff/tasks",
+    icon: BiTask,
+  },
+];
+
+const MASTER_ROUTES = [
+  {
+    name: "Measure Unit",
+    path: "/master/measure-unit",
+    icon: BiRuler,
+  },
+  {
+    name: "Clothing Type",
+    path: "/master/clothing-type",
+    icon: BiRuler,
+  },
+  {
+    name: "Fabric Type",
+    path: "/master/fabric-type",
+    icon: BiRuler,
+  },
+];
+
+export { STAFF_ROUTES, MASTER_ROUTES };
