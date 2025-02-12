abdca3d5470c9840e29480ca12de9de34e8f6a5f
Alish Baig <alishbaig2004@gmail.com>
2025-02-12 20:17:54 +0530

    refactor: simplify RootComponent by removing unnecessary layout elements

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,27 +1,9 @@
-import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { Container, Flex, VStack } from "@yamada-ui/react";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <Flex width="full" height="full">
-      <Sidebar />
-      <VStack gap="0">
-        <Navbar />
-        <Container
-          width="full"
-          height="full"
-          css={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-        >
-          <Outlet />
-        </Container>
-      </VStack>
-    </Flex>
-  );
+  return <Outlet />;
 }
