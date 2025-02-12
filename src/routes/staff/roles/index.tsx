039b7af696b4d6ef31ed38b491f01393f534265b
Alish Baig <alishbaig2004@gmail.com>
2025-02-12 20:18:35 +0530

    refactor: remove unused routes and components for about, clothing type, fabric type, measure unit, and staff

--- src/routes/staff/roles/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/staff/roles/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/staff/roles/"!</div>
-}
--- src/routes/staff/tasks/$taskId.edit.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/staff/tasks/$taskId/edit")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/staff/tasks/$taskId/edit"!</div>;
-}
