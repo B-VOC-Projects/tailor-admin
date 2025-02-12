4e012749ebd95df316f9b86e612e621c0ebd6ca3
Alish Baig <alishbaig2004@gmail.com>
2025-02-12 20:19:40 +0530

    refactor: remove unused staff tasks route and related code

--- src/routes/staff/tasks/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/staff/tasks/")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/staff/tasks/"!</div>;
-}
